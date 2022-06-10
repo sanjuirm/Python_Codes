f578c7f6436eedc50f77e7a4ae993fa6f9ae28bf
Sanju Halder <work.sanju98@gmail.com>
2022-06-11 00:51:36 +0530

    Delete Chapter_02 directory

--- Chapter_02/03.py
+++
@@ -1,9 +0,0 @@
-# A program to compute average of three numbers.
-
-def main():
-    print('This Program calculate the average of three numbers.')
-    x,y,z=eval(input('Enter three numbers(separated by comma):'))
-    avg=(x+y+z)/3
-
-    print('The average is:', avg)
-main()
