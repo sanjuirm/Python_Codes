fff0ecf855a89ab56ff819570c111dde203eb0ac
Sanju Halder <work.sanju98@gmail.com>
2022-06-30 11:41:47 +0530

    Add files via upload

---
+++ Chapter_08/05.py
@@ -0,0 +1,17 @@
+import math as m
+import pyinputplus as pip
+
+def main():
+    num = pip.inputInt(prompt= 'Enter a number: ', min=3)
+    
+    y = int(m.sqrt(num))
+    i = 2
+     
+    while i <= y:
+        if num % i == 0:
+            print('It\'s  not a Prime Number')
+            break
+        else:
+            i += 1
+        print('Prime Number')
+main()
