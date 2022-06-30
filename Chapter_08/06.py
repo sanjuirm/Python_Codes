2b6df6390998d4104850420fc955888262d6f85a
Sanju Halder <work.sanju98@gmail.com>
2022-06-30 14:09:03 +0530

    Add files via upload

---
+++ Chapter_08/06.py
@@ -0,0 +1,26 @@
+import math as m
+import pyinputplus as pip
+
+def main():
+    num = pip.inputInt(prompt= 'Enter a number: ', min=3)
+    
+    
+    
+    while num > 1:
+        flag = True
+        y = int(m.sqrt(num))
+        i = 2
+
+        while i <= y:
+            if num % i == 0:
+                flag = False
+                break
+            else:
+                i += 1
+
+        if flag :
+            print(num)
+
+        num = num - 1 
+
+main()
